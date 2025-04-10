4831705201ad779f1a83c210d87f0f3cc630a764
George-Samaan <georgemichel07@icloud.com>
2025-04-10 17:59:49 +0200

    Add animated navigation between CityInput and CombinedWeather screens

--- app/src/main/java/com/vodafonetask/weathernowandlater/navigation/AppNavGraph.kt
+++ app/src/main/java/com/vodafonetask/weathernowandlater/navigation/AppNavGraph.kt
@@ -1,5 +1,10 @@
 package com.vodafonetask.weathernowandlater.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -11,13 +16,42 @@ import com.vodafonetask.weathernowandlater.ui.CombinedWeatherScreen
 fun AppNavGraph(navController: NavHostController) {
     NavHost(navController, startDestination = "cityInput") {
 
-        composable("cityInput") {
+        composable(
+            route = "cityInput",
+            enterTransition = { fadeIn(animationSpec = tween(300)) },
+            exitTransition = { fadeOut(animationSpec = tween(300)) })
+        {
             CityInputScreen { cityName ->
                 navController.navigate("combinedScreen/$cityName")
             }
         }
 
-        composable(route = "combinedScreen/{city}") { backStackEntry ->
+        composable(
+            route = "combinedScreen/{city}",
+            enterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { it },
+                    animationSpec = tween(500)
+                )
+            },
+            exitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { -it },
+                    animationSpec = tween(500)
+                )
+            },
+            popEnterTransition = {
+                slideInHorizontally(
+                    initialOffsetX = { -it }, animationSpec = tween(500)
+                )
+            },
+            popExitTransition = {
+                slideOutHorizontally(
+                    targetOffsetX = { it },
+                    animationSpec = tween(500)
+                )
+            }
+        ) { backStackEntry ->
             val city = backStackEntry.arguments?.getString("city") ?: return@composable
             CombinedWeatherScreen(city)
         }
